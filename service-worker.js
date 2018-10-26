476b3e2ba292bdb19255dea26ee7a1df64c8c196
omer.sheikh <omer.sheikh@afiniti.com>
2018-10-27 01:35:28 +0500

    Fixed Workbox config position

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,46 @@
-importScripts("/precache-manifest.11a08ca86c3733869845e2b243753d89.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts(
+  "/precache-manifest.11a08ca86c3733869845e2b243753d89.js",
+  "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
+);
 
 if (workbox) {
   console.log(`Workbox is loaded`);
-
+  workbox.setConfig({
+    debug: false
+  });
   workbox.precaching.precacheAndRoute(self.__precacheManifest);
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: "images",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+        })
+      ]
+    })
+  );
+
+  workbox.routing.registerRoute(
+    new RegExp("https://api.flickr.com/services/rest"),
+    workbox.strategies.networkFirst({
+      cacheName: "api"
+    })
+  );
+
+  workbox.routing.registerRoute(
+    new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+    workbox.strategies.cacheFirst({
+      cacheName: "googleapis",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 30
+        })
+      ]
+    })
+  );
 } else {
   console.log(`Workbox didn't load`);
 }
-
-workbox.setConfig({
-  debug: false
-});
-
-workbox.precaching.precacheAndRoute([]);
-
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "images",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
-      })
-    ]
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp("https://api.flickr.com/services/rest"),
-  workbox.strategies.networkFirst({
-    cacheName: "api"
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
-  workbox.strategies.cacheFirst({
-    cacheName: "googleapis",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 30
-      })
-    ]
-  })
-);
 
